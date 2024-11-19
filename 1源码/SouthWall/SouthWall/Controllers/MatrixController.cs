3d677f85e79f0e115805906a094db7f371b230a4
TerryZou1989 <terryzou1989@163.com>
2024-11-19 14:17:17 +0800

    后台管理页面设计

---
+++ 1源码/SouthWall/SouthWall/Controllers/MatrixController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SouthWall.Controllers
+{
+    public class MatrixController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Times()
+        {
+            return View();
+        }
+        public IActionResult Videos()
+        {
+            return View();
+        }
+        public IActionResult Articles()
+        {
+            return View();
+        }
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
