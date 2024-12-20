85f6016edfd989026b933d1c18a5f77d3ce16b46
Terry Zou <terryzou1989@163.com>
2024-12-20 11:06:38 +0800

    增加公众号二维码

---
+++ 1源码/SouthWall/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using OpenAI.Chat;
+
+ChatClient client = new(
+    model: "gpt-4o",
+    apiKey: "123456");
+ChatCompletion completion = client.CompleteChat("Say 'this is a test.'");
+
+Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
